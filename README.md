1d2efc64b2014802e9697cd7da93588db754fba3
SOsintOps <28706121+SOsintOps@users.noreply.github.com>
2022-09-19 14:51:34 +0200

    The  VM project

--- README.md
+++ README.md
@@ -1,2 +1,114 @@
-# Artemis
-This script will automatically set up an OSINT workstation starting from a Ubuntu OS. 
+# OSINT ITALIA OSINT VM
+<img align="right" width="86" height="150" src="https://github.com/osintitalia/Artemis/blob/master/multimedia/images/osintitalia_logo.jpeg">
+
+This script will automatically set up an open source intelligence (OSINT) workstation starting from a clean Ubuntu* 22.04 LTS Virtual Machine/Workstation.\
+Best practices recommend using a new VM for each OSInt investigation.\
+There are several ready-made VMs available on the Internet. I wanted to study how to customise my own workstation following what Michael Bazzell suggest in his own book, [Open Source Intelligence Techniques - 9th Edition (2022)](https://inteltechniques.com/book1.html), about creating your own custom VM.\
+This script will setup the workstation by installing mostly of the scripts suggested by Michael and more and more other tools will be added in the future.
+
+## INDEX
+- [REQUIREMENTS](https://github.com/osintitalia/Artemis/blob/master/README.md#REQUIREMENTS)
+- [TOOLS](https://github.com/osintitalia/Artemis/blob/master/README.md#TOOLS)
+- [INSTALLING & RUNNING](https://github.com/osintitalia/Artemis#installing--running)
+- [TO DO](https://github.com/osintitalia/Artemis#to-do)
+- [RESOURCES](https://github.com/osintitalia/Artemis/blob/master/README.md#resources)
+- [CREDITS](https://github.com/osintitalia/Artemis#credits)
+- [LICENSES](https://github.com/osintitalia/Artemis#licenses)
+
+
+## REQUIREMENTS
+To install and run Artemis, you need:
+- an Ubuntu 22.04 LTS virtual machine/workstation
+- set your user on "**osint**"
+- Please use English Language!
+
+This script implies that you rely on VmWare for virtualization. Just in case, I included a commented out portion of code which will install virtual box tools.
+
+**N.B.**: This script has been tested on:
+- [Ubuntu](https://ubuntu.com/download) 22.04 LTS VM.
+- [Ubuntu](https://ubuntu.com/download) 20.04 LTS VM (Firefox will not be customised).
+
+## TOOLS
+
+### OSInt
+- [Amass](https://github.com/OWASP/Amass)
+- [instaloader](https://instaloader.github.io/)
+- [InstaLooter](https://github.com/althonos/InstaLooter)
+- [Toutatis](https://github.com/megadose/toutatis)
+- [HTTrack](https://www.httrack.com/)
+- [MediaInfo](https://mediaarea.net/en/MediaInfo/Download/Ubuntu)
+- [ExifTool](https://github.com/pandastream/libimage-exiftool-perl-9.27)
+- [EyeWitness](https://github.com/ChrisTruncer/EyeWitness)
+- [sublist3r](https://github.com/aboul3la/Sublist3r)
+- [Photon](https://github.com/s0md3v/Photon)
+- [The Harvester](https://github.com/laramies/theHarvester)
+- [Metagoofil](https://github.com/opsdisk/metagoofil)
+- [recon-ng](https://github.com/lanmaster53/recon-ng)
+- [sherlock](https://github.com/sherlock-project/sherlock)
+- [spiderfoot](https://github.com/smicallef/spiderfoot)
+- [webosint](https://github.com/C3n7ral051nt4g3ncy/webosint)
+- [blackbird](https://github.com/p1ngul1n0/blackbird)
+- [Elasticsearch-Crawler](https://github.com/AmIJesse/Elasticsearch-Crawler)
+- [Ripgrep](https://github.com/BurntSushi/ripgrep)
+- [holehe](https://github.com/megadose/holehe)
+- [kali-anonstealth](https://github.com/Und3rf10w/kali-anonsurf)
+- [Moriarty-Project V2.6](https://github.com/AzizKpln/Moriarty-Project)
+- [maigret](https://github.com/soxoj/maigret)
+- [Maltego](https://www.maltego.com/)
+- and more!
+
+### General Purpose tools
+- [VLC](https://www.videolan.org/vlc/index.html)
+- [Google Earth](https://www.google.com/earth/versions/#earth-pro)
+- [wget](https://www.gnu.org/software/wget/)
+- [cherrytree](https://www.giuspen.com/cherrytree/)
+- [Atom](https://atom.io/)
+- [KeepassXC](https://keepassxc.org/)
+- [Kazam](https://launchpad.net/kazam)
+- [Audacity](https://www.audacityteam.org/)
+- [Ripgrep](https://github.com/BurntSushi/ripgrep)
+- [Tor Browser](https://www.torproject.org/)
+- [OpenShot](https://www.openshot.org/)
+- [Obisidian](https://obsidian.md/) Please refer to this article [1](https://webbreacher.com/2022/03/15/obsidian/)
+- [Threat Intelligence Resources](https://github.com/pstirparo/threatintel-resources) Please refer to those articles [1](https://isc.sans.edu/forums/diary/Analysis+of+Competing+Hypotheses+ACH+part+1/22460/) and [2](https://isc.sans.edu/forums/diary/Analysis+of+Competing+Hypotheses+WCry+and+Lazarus+ACH+part+2/22470/)
+- and more!
+
+
+## INSTALLING & RUNNING
+1) Open the Terminal app
+
+2) Make sure and/or install that the [GIT command](https://linuxize.com/post/how-to-install-git-on-ubuntu-20-04/) is available in your VM:
+    ```bash
+    sudo apt install -y git
+    ```
+3) Clone this repository in the ```/Download/``` directory:
+    ```bash
+    git clone https://github.com/osintitalia/Artemis ~/Downloads/Artemis
+    ```
+
+4) make the setup script executable:
+    ```bash
+    sudo chmod +x ~/Downloads/Artemis/setup.sh
+    ```
+
+5) Close FireFox and run the file:
+    ```bash
+    ~/Downloads/Artemis/setup.sh
+    ```
+
+## TO DO
+- Add more functionality to the existing bash shortcuts, such as predefined spiderfoot searches or RiskIQ CLI.
+- set osint report templates as libreoffice's templates.
+- customise the user profile.
+- add Nmap/zenmap, electrum wallet, gpa gui for PGP keys, Openshot video editor, [hunchly](https://www.hunch.ly/), etc.
+
+## RESOURCES
+- [Osint Italia website](https://osintitalia.it)
+- [OSInt Daily News](https://t.me/Osintlatestnews)
+- [Open Source Intelligence Techniques - 9th Edition (2022) - Michael Bazzell](https://inteltechniques.com/book1.html)
+
+## CREDITS
+- [SOsintOps](https://github.com/SOsintOps) and [stanleyhal](https://github.com/stanleyhal) for developing and Customising the [Argos](https://github.com/SOsintOps/Argos) script suggesting the script's name!
+
+## LICENSES
+Please refer to the license files.
---
+++ setup.sh
@@ -0,0 +1,336 @@
+#!/bin/bash
+
+
+OKBLUE='\033[94m'
+OKRED='\033[91m'
+OKGREEN='\033[92m'
+OKORANGE='\033[93m'
+RESET='\e[0m'
+
+echo -e "$OKBLUE          _                   _           _              _            _        "
+echo -e "$OKBLUE         / /\                /\ \        /\ \           /\ \         / /\      "
+echo -e "$OKBLUE        / /  \              /  \ \      /  \ \         /  \ \       / /  \     "
+echo -e "$OKBLUE       / / /\ \            / /\ \ \    / /\ \_\       / /\ \ \     / / /\ \__  "
+echo -e "$OKBLUE      / / /\ \ \          / / /\ \_\  / / /\/_/      / / /\ \ \   / / /\ \___\ "
+echo -e "$OKBLUE    / / /  \ \ \        / / /_/ / / / / / ______   / / /  \ \_\  \ \ \ \/___/ "
+echo -e "$OKBLUE    / / /___/ /\ \      / / /__\/ / / / / /\_____\ / / /   / / /   \ \ \       "
+echo -e "$OKBLUE   / / /_____/ /\ \    / / /_____/ / / /  \/____ // / /   / / /_    \ \ \      "
+echo -e "$OKBLUE  / /_________/\ \ \  / / /\ \ \  / / /_____/ / // / /___/ / //_/\__/ / /      "
+echo -e "$OKBLUE / / /_       __\ \_\/ / /  \ \ \/ / /______\/ // / /____\/ / \ \/___/ /       "
+echo -e "$OKBLUE \_\___\     /____/_/\/_/    \_\/\/___________/ \/_________/   \_____\/        "
+echo "    "
+echo -e "$RESET"
+echo -e "$OKRED                              +----=[Osint Italia]=----+ $RESET"
+
+
+echo '#######################################################################'
+echo '#                         Let me update your OS                       #'
+echo '#######################################################################'
+
+sudo apt -y update && sudo apt -y upgrade
+sudo snap refresh
+
+# Optional Virtualbox tools
+# sudo add-apt-repository multiverse
+# sudo apt install virtualbox-guest-dkms virtualbox-guest-x11
+# sudo adduser osint vboxsf
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+
+echo '#######################################################################'
+echo '#                            A few scripts                           #'
+echo '#######################################################################'
+
+
+mkdir ~/Documents/scripts && cp ~/Downloads/Artemis/scripts/* ~/Documents/scripts
+sudo chmod +x ~/Documents/scripts/*.sh
+
+mkdir ~/Pictures/icons && cp ~/Downloads/Artemis/multimedia/icons/* ~/Pictures/icons
+
+sudo cp ~/Downloads/Artemis/shortcuts/* /usr/share/applications/
+
+# this command will copy all the OSINT templates to the Templates Directory. I may need to delete o move to another directory all the Templates directory's default templates
+cp -r ~/Downloads/Artemis/templates/* ~/Templates
+
+# setting the 'quiet priest' background image
+cp ~/Downloads/Artemis/multimedia/wallpapers/* ~/Pictures && ~/Documents/scripts/background.sh
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+
+
+echo '#######################################################################'
+echo '#      Requirements for installing tools, scripts and launchers       #'
+echo '#######################################################################'
+sudo apt install -y vlc
+sudo apt install -y python3
+sudo apt install -y python-setuptools
+sudo apt install -y jq
+sudo apt install -y python3-pip
+sudo apt install -y git
+sudo apt install -y curl
+sudo apt install -y wget
+sudo apt install -y curl
+sudo apt install -y webhttrack
+sudo apt install -y ffmpeg
+sudo apt install -y mediainfo-gui
+sudo apt install -y libimage-exiftool-perl
+sudo apt install -y subversion
+sudo -H pip3 install --upgrade youtube_dl
+sudo -H pip3 install Instalooter
+sudo -H pip3 install Instaloader
+sudo -H pip3 install toutatis
+sudo python3 -m pip install pipenv
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+
+echo '#######################################################################'
+echo '#                          Customizing Firefox                        #'
+echo '#######################################################################'
+
+# firefox &
+# sleep 30
+pkill -f firefox
+# rm -rfv ~/snap/firefox/common/.mozilla/firefox/*.default
+cd ~/Downloads/Artemis/ffprofile/
+zip -F ffprofile.zip --out Artemis-ff-template2.zip
+unzip Artemis-ff-template2.zip
+cp -R ~/Downloads/Artemis/ffprofile/Artemis-ff-template/* ~/snap/firefox/common/.mozilla/firefox/*.default
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                           Obsidian                                  #'
+echo '#######################################################################'
+cd ~/Downloads
+wget https://github.com/obsidianmd/obsidian-releases/releases/download/v0.14.6/obsidian_0.14.6_amd64.snap
+sudo snap install --dangerous obsidian_0.14.6_amd64.snap
+sudo rm obsidian_0.14.6_amd64.snap
+git clone https://github.com/WebBreacher/obsidian-osint-templates ~/Documents/obsidian-osint-templates #template osint di obsidian
+git clone https://github.com/theNerdInTheHighCastle/Obsidian ~/Documents/obsidian-criptovalute #template osint di obsidian
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                             Amass                                   #'
+echo '#######################################################################'
+sudo snap install amass
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                          EyeWitness                                 #'
+echo '#######################################################################'
+
+git clone https://github.com/FortyNorthSecurity/EyeWitness.git ~/Downloads/Programs/EyeWitness
+sudo -H ~/Downloads/Programs/EyeWitness/Python/setup/setup.sh
+cd ~/Documents/scripts
+sed -i "s/Programs\/EyeWitness/Programs\/EyeWitness\/Python/g" eyewitness.sh
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                           sublist3r                                 #'
+echo '#######################################################################'
+
+git clone https://github.com/aboul3la/Sublist3r.git ~/Downloads/Programs/Sublist3r
+sudo -H pip3 install -r ~/Downloads/Programs/Sublist3r/requirements.txt
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                              Photon                                 #'
+echo '#######################################################################'
+
+git clone https://github.com/s0md3v/Photon.git ~/Downloads/Programs/Photon
+sudo -H pip3 install -r ~/Downloads/Programs/Photon/requirements.txt
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                       The Harvester                                 #'
+echo '#######################################################################'
+
+git clone https://github.com/laramies/theHarvester.git ~/Downloads/Programs/theHarvester
+cd ~/Downloads/Programs/theHarvester
+sudo -H pip3 install -r requirements/base.txt
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                           Metagoofil                                #'
+echo '#######################################################################'
+
+git clone https://github.com/opsdisk/metagoofil.git ~/Downloads/Programs/metagoofil
+sudo -H pip3 install -r ~/Downloads/Programs/metagoofil/requirements.txt
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                              recon-ng                               #'
+echo '#######################################################################'
+
+git clone https://github.com/lanmaster53/recon-ng.git ~/Downloads/Programs/recon-ng
+cd ~/Downloads/Programs/recon-ng
+sudo -H pip3 install -r REQUIREMENTS
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                              sherlock                               #'
+echo '#######################################################################'
+
+git clone https://github.com/sherlock-project/sherlock.git ~/Downloads/Programs/sherlock
+python3 -m pip install -r ~/Downloads/Programs/sherlock/requirements.txt
+cd ~/Documents/scripts/
+sed -i 's/Programs\/sherlock/Programs\/sherlock\/sherlock/g' sherlock.sh
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                            spiderfoot                               #'
+echo '#######################################################################'
+
+git clone https://github.com/smicallef/spiderfoot.git ~/Downloads/Programs/spiderfoot
+sudo -H pip3 install -r ~/Downloads/Programs/spiderfoot/requirements.txt
+cd ~/Documents/scripts
+sed -i '/^sudo/ d' spiderfoot.sh
+sed -i '/^firefox/ d' spiderfoot.sh
+echo "cd ~/Downloads/Programs/spiderfoot" >> spiderfoot.sh
+echo "python3 sf.py -l 127.0.0.1:5001 &" >> spiderfoot.sh
+echo "sleep 5" >> spiderfoot.sh
+echo "firefox http://127.0.0.1:5001" >> spiderfoot.sh
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                             W3b0s1nt                                #'
+echo '#######################################################################'
+
+git clone https://github.com/C3n7ral051nt4g3ncy/webosint.git ~/Downloads/Programs/webosint
+cd ~/Downloads/Programs/webosint
+sudo -H pip3 install -r requirements.txt
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                             blackbird                               #'
+echo '#######################################################################'
+
+git clone https://github.com/p1ngul1n0/blackbird.git ~/Downloads/Programs/blackbird
+cd ~/Downloads/Programs/blackbird
+sudo -H pip3 install -r requirements.txt
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                     Elasticsearch-Crawler                           #'
+echo '#######################################################################'
+
+git clone https://github.com/AmIJesse/Elasticsearch-Crawler.git ~/Downloads/Programs/Elasticsearch-Crawler
+cd ~/Downloads/Programs/Elasticsearch-Crawler
+sudo -H pip3 install nested-lookup
+sudo -H pip3 install internetarchive
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                               Ripgrep                               #'
+echo '#######################################################################'
+
+cd ~/Downloads/
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
+sudo dpkg -i ripgrep_11.0.2_amd64.deb
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                                holehe                               #'
+echo '#######################################################################'
+
+git clone https://github.com/megadose/holehe.git ~/Downloads/Programs/holehe
+cd ~/Downloads/Programs/holehe && sudo python3 setup.py install
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                              anonsurf                               #'
+echo '#######################################################################'
+
+git clone https://github.com/Und3rf10w/kali-anonsurf.git ~/Downloads/Programs/kali-anonsurf
+cd ~/Downloads/Programs/kali-anonsurf && sudo ./installer.sh
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                        Moriarty-Project                             #'
+echo '#######################################################################'
+
+git clone https://github.com/AzizKpln/Moriarty-Project ~/Downloads/Programs/Moriarty-Project
+cd ~/Downloads/Programs/Moriarty-Project && chmod 755 install.sh && bash install.sh
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                               maigret                               #'
+echo '#######################################################################'
+
+git clone https://github.com/soxoj/maigret ~/Downloads/Programs/maigret
+pip3 install ~/Downloads/Programs/maigret/.
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                           Google Earth                              #'
+echo '#######################################################################'
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo wget -O google-earth64.deb http://dl.google.com/dl/earth/client/current/google-earth-stable_current_amd64.deb
+sudo dpkg -i google-earth64.deb && sudo apt -f install && sudo rm google-earth64.deb
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                               Maltego                               #'
+echo '#######################################################################'
+
+wget -O maltego.deb https://maltego-downloads.s3.us-east-2.amazonaws.com/linux/Maltego.v4.3.0.deb
+sudo dpkg -i maltego.deb && sudo apt -f install && rm maltego.deb
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+echo '#######################################################################'
+echo '#                       Customising scripts                           #'
+echo '#######################################################################'
+cd ~/Documents/scripts/
+sed -i "s/python3\.6/python3/g" domains.sh
+sed -i "s/python\ sublist3r/python3\ sublist3r/g" domains.sh
+# sed -i "s/pip\ install/pip3\ install/g" updates.sh
+# sed -i "s/sudo\ pip3/sudo\ \-H\ pip3/g" updates.sh
+sudo pip3 install --upgrade pip
+cd ~/Downloads/Programs
+echo -e "$OKGREEN +----=[DOOONE!!!]=----+ $RESET"
+echo "    "
+
+echo '#######################################################################'
+echo '#                       General Purpose tools                         #'
+echo '#######################################################################'
+sudo add-apt-repository -y ppa:giuspen/ppa && sudo apt install -y cherrytree
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add - && sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list' && sudo apt update && sudo apt install -y atom
+sudo apt install -y keepassxc
+sudo apt install -y torbrowser-launcher
+sudo apt install -y kazam
+sudo apt install -y audacity
+sudo apt install -y openjdk-11-jre
+sudo apt install -y ripgrep
+sudo apt install -y p7zip unrar
+sudo apt install -y openshot
+sudo apt install -y open-vm-tools
+#sudo apt install -y ubuntu-restricted-extras
+git clone https://github.com/pstirparo/threatintel-resources ~/Documents/threatintel-resources
+sudo apt autoremove -y
+echo -e "$OKRED +----=[Si vis pacem, para bellum]=----+ $RESET"
+echo "    "
+read -rsp $'Press enter to reboot...\n'
+sudo reboot now
